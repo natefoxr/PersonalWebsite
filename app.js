46b154328511014135c893b38489165e242b34e9
natefoxr <nate@foxr.xyz>
2022-07-04 02:40:21 -0700

    Added content to myCode through jQuery and GitHub API

--- app.js
+++ app.js
@@ -11,4 +11,34 @@ $(document).ready(function() {
         $('.changelink-m').replaceWith('<a class="nav-link text-light fs-4 px-3 hoverm changelink-m" href="./myCode.html">My Code</a>')
         $('.changelink-r').replaceWith('<a class="nav-link text-light fs-4 px-3 hoverr changelink-r" href="./resume.html">Resume</a>')
     }
+    $.get('https://api.github.com/users/natefoxr/repos', function(data) {
+        data.forEach(element => {
+            let description = element['description']
+            if(description === null) {
+                description = 'A project by FOXR'
+            }
+            let language = element['language']
+            if(language === null) {
+                language = 'N/A'
+            }
+            if(element['fork'] == false){
+                $('.project-cards').append(
+                    `<div class="col-md-6 col-xl-4">
+                        <div class="card mx-md-4 my-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${element['name']}</h5>
+                                <p class="card-text">${description}</p>
+                                <p class="card-text">Created at ${element['created_at'].substring(0,10)}</p>
+                                <p class="card-text">Last update ${element['updated_at'].substring(0,10)}</p>
+                                <p class="card-text">Last push ${element['pushed_at'].substring(0,10)}</p>
+                                <p class="card-text">Language: ${language}</p>
+    
+                                <a href="${element['html_url']}" target="_blank" class="btn btn-primary">Go to GitHub.com</a>
+                            </div>
+                        </div>
+                    </div>`
+                )
+            }
+      })
+    })
 })
