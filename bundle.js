0d0c9c9fc098dd403f4b6dc76d9a84392be6866d
natefoxr <nate@foxr.xyz>
2022-07-07 22:46:03 -0700

    Added connection to the ETH blockchain and set alerts/buttons if Metamask not installed

--- bundle.js
+++ bundle.js
@@ -32325,6 +32325,10 @@ function extend() {
 
 },{}],243:[function(require,module,exports){
 $(document).ready(function() {
+    if (typeof window.ethereum == 'undefined') {
+        $('.eth-10').replaceWith('<button class="eth-5 rounded fw-bold border border-0 my-3 p-2"><a href="https://metamask.io/download/" target="_blank" class="text-decoration-none text-dark">Install Metamask</a></button>')
+        $('.eth-5').replaceWith('<button class="eth-5 rounded fw-bold border border-0 my-3 p-2"><a href="https://metamask.io/download/" target="_blank" class="text-decoration-none text-dark">Install Metamask</a></button>')
+    }      
     const development = true;
     if(development == true) {
         // local server
@@ -32379,11 +32383,11 @@ $(document).ready(function() {
         console.log("Email: ", email);
         console.log("Message: ", message);
     })
-    // #TODO: Make the pay buttons be install metamask on ready if not installed
     $('.eth-5').on('click', function() {
         if (typeof window.ethereum !== 'undefined') {
             var Web3 = require('web3');
             var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+            ethereum.request({ method: 'eth_requestAccounts' });
             web3.eth.getAccounts()
                 .then(e => {
                     let account=e[0]
@@ -32391,22 +32395,26 @@ $(document).ready(function() {
             })
          }
         else{
-                console.log('MetaMask not installed!');
+            alert("Please install MetaMask to use this function!");
+            console.log('MetaMask not installed!');
         }
     })
     $('.eth-10').on('click', function() {
         if (typeof window.ethereum !== 'undefined') {
             var Web3 = require('web3');
             var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+            ethereum.request({ method: 'eth_requestAccounts' });
             web3.eth.getAccounts()
                 .then(e => {
                     let account=e[0]
-                    $('.eth-text').text(`Connected with account ${account.substring(0,4)}...${account.substring(account.length - 4)}`)
+                    // $('.eth-text').text(`Connected with account ${account.substring(0,4)}...${account.substring(account.length - 4)}`)
             })
          }
         else{
-                console.log('MetaMask not installed!');
+            alert("Please install MetaMask to use this function!");
+            console.log('MetaMask not installed!');
         }
+        $('.eth-text').text(`Connected with account ${account.substring(0,4)}...${account.substring(account.length - 4)}`)
     })
 });
 
